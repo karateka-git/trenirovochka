237845cbfc6aad2816544dc53be6c595f430adc3
Erofeev Vladislav <v.erofeev@ltst.ru>
2022-07-06 16:05:25 +0600

    VE: add binding to project

--- app/src/main/java/com/example/trenirovochka/MainActivity.kt
+++ app/src/main/java/com/example/trenirovochka/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.trenirovochka
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.trenirovochka.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
     }
 }

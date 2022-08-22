5315409d73ab59f7c5b2ed29887947eb9ea423bd
Erofeev Vladislav <v.erofeev@ltst.ru>
2022-08-22 14:59:50 +0600

    VE: add clear focus on touch outside EditText

--- app/src/main/java/com/example/trenirovochka/presentation/screens/main/MainActivity.kt
+++ app/src/main/java/com/example/trenirovochka/presentation/screens/main/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.trenirovochka.presentation.screens.main
 
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.widget.EditText
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.trenirovochka.databinding.ActivityMainBinding
+import com.example.trenirovochka.presentation.common.extensions.hideKeyboard
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -21,5 +26,20 @@ class MainActivity : AppCompatActivity() {
             setContentView(it.root)
         }
         navController = binding.navHostFragment.getFragment<NavHostFragment>().navController
+    }
+
+    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            val currentFocusView: View? = currentFocus
+            if (currentFocusView is EditText) {
+                val outRect = Rect()
+                currentFocusView.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+                    currentFocusView.clearFocus()
+                    binding.root.hideKeyboard()
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event)
     }
 }

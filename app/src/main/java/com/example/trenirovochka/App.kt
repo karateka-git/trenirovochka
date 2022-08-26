9577e77ccfd751d7f493e7703339a9368a16ba47
Erofeev Vladislav <v.erofeev@ltst.ru>
2022-08-26 14:10:19 +0600

    VE: add material 3 dynamical color for theme

--- app/src/main/java/com/example/trenirovochka/App.kt
+++ app/src/main/java/com/example/trenirovochka/App.kt
@@ -1,9 +1,20 @@
 package com.example.trenirovochka
 
 import android.app.Application
+import com.google.android.material.color.DynamicColors
+import com.google.android.material.color.DynamicColorsOptions
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class App : Application() {
-    // TODO
+
+    override fun onCreate() {
+        super.onCreate()
+        // Apply dynamic color
+        DynamicColors.applyToActivitiesIfAvailable(
+            this,
+            DynamicColorsOptions.Builder().setThemeOverlay(R.style.Theme_Trenirovochka_Overlay)
+                .build()
+        )
+    }
 }

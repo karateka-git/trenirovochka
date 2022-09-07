4238ae70c1c0a3070f4abe8512122d890c67accd
Erofeev Vladislav <zv97@mail.ru>
2022-09-07 18:34:48 +0600

    VE: add trainingProgram to mock repository

--- app/src/main/java/com/example/trenirovochka/data/remote/repositories/TrainingProgramRemoteRepositoryMock.kt
+++ app/src/main/java/com/example/trenirovochka/data/remote/repositories/TrainingProgramRemoteRepositoryMock.kt
@@ -7,6 +7,7 @@ import com.example.trenirovochka.domain.models.TrainingProgram
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
+import java.time.DayOfWeek
 import java.util.*
 import javax.inject.Inject
 import javax.inject.Named
@@ -16,49 +17,96 @@ class TrainingProgramRemoteRepositoryMock @Inject constructor(
     private val calendar: Calendar,
     // TODO add service
 ) : ITrainingProgramRemoteRepository {
-    override fun getTrainingProgram(date: String) = flow {
-        emit(
-            TrainingProgram(
-                formatAsFullDate(calendar.time),
-                "test name",
-                listOf(
-                    Exercise(
-                        "Ги",
-                        3,
-                        0,
-                        10,
-                        "5",
-                    ),
-                    Exercise(
-                        "Гипер",
-                        3,
-                        0,
-                        10,
-                        "5",
-                    ),
-                    Exercise(
-                        "Гиперэкс",
-                        3,
-                        0,
-                        10,
-                        "5",
-                    ),
-                    Exercise(
-                        "Гиперэкстен",
-                        3,
-                        0,
-                        10,
-                        "5",
-                    ),
-                    Exercise(
-                        "Гиперэкстензия",
-                        3,
-                        0,
-                        10,
-                        "5",
-                    )
+    private val trainingProgramList = listOf(
+        TrainingProgram(
+            formatAsFullDate(Calendar.getInstance().time),
+            "test name",
+            listOf(
+                Exercise(
+                    "Ги",
+                    3,
+                    0,
+                    10,
+                    "5",
+                ),
+                Exercise(
+                    "Гипер",
+                    3,
+                    0,
+                    10,
+                    "5",
+                ),
+                Exercise(
+                    "Гиперэкс",
+                    3,
+                    0,
+                    10,
+                    "5",
+                ),
+                Exercise(
+                    "Гиперэкстен",
+                    3,
+                    0,
+                    10,
+                    "5",
+                ),
+                Exercise(
+                    "Гиперэкстензия",
+                    3,
+                    0,
+                    10,
+                    "5",
+                )
+            )
+        ),
+        TrainingProgram(
+            formatAsFullDate(Calendar.getInstance().apply { add(Calendar.DAY_OF_YEAR, 1) }.time),
+            "test name",
+            listOf(
+                Exercise(
+                    "По",
+                    3,
+                    0,
+                    10,
+                    "5",
+                ),
+                Exercise(
+                    "Подтя",
+                    3,
+                    0,
+                    10,
+                    "5",
+                ),
+                Exercise(
+                    "Подтяги",
+                    3,
+                    0,
+                    10,
+                    "5",
+                ),
+                Exercise(
+                    "Подтягива",
+                    3,
+                    0,
+                    10,
+                    "5",
+                ),
+                Exercise(
+                    "Подтягивания",
+                    3,
+                    0,
+                    10,
+                    "5",
                 )
             )
         )
+    )
+
+    override fun getTrainingProgram(date: String) = flow {
+        trainingProgramList.find { it.date == date }?.let {
+            emit(
+                it
+            )
+        }
     }.flowOn(ioDispatcher)
 }

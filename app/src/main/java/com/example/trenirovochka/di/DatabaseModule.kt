f9f9c50145b7cb93493d730f402a6f74a21379f8
Erofeev Vladislav <zv97@mail.ru>
2022-10-10 16:10:23 +0600

    VE: add pre-populate database

--- app/src/main/java/com/example/trenirovochka/di/DatabaseModule.kt
+++ app/src/main/java/com/example/trenirovochka/di/DatabaseModule.kt
@@ -2,24 +2,48 @@ package com.example.trenirovochka.di
 
 import android.content.Context
 import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.trenirovochka.data.local.storage.StorageDatabase
 import com.example.trenirovochka.data.local.storage.daos.TrainingPlanDao
+import com.example.trenirovochka.data.local.storage.entities.TrainingPlanEntity
+import com.example.trenirovochka.data.local.storage.entities.TrainingPlanJoinEntity
+import com.example.trenirovochka.domain.models.DaysOfWeek
+import com.example.trenirovochka.domain.models.TrainingDay
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import javax.inject.Provider
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 class DatabaseModule {
+
+    private val applicationScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+
     @Singleton
     @Provides
     fun provideStorageDatabase(
         @ApplicationContext context: Context,
-    ): StorageDatabase =
-        Room.databaseBuilder(context, StorageDatabase::class.java, "storage_database.db").build()
+        trainingPlanProvider: Provider<TrainingPlanDao>
+    ): StorageDatabase {
+        val database: StorageDatabase =
+            Room.databaseBuilder(context, StorageDatabase::class.java, "storage_database.db")
+                .addCallback(PrepopulateTrainingPlanCallback(trainingPlanProvider)).build()
+
+        // call to create a database
+        applicationScope.launch {
+            database.runInTransaction {}
+        }
+        return database
+    }
 
     @Singleton
     @Provides
@@ -27,4 +51,30 @@ class DatabaseModule {
         database: StorageDatabase,
     ): TrainingPlanDao =
         database.trainingPlanDao()
+
+    inner class PrepopulateTrainingPlanCallback(
+        private val trainingPlanProvider: Provider<TrainingPlanDao>
+    ) : RoomDatabase.Callback() {
+
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            applicationScope.launch {
+                populateDatabase()
+            }
+        }
+
+        // populate default plans
+        private suspend fun populateDatabase() {
+            trainingPlanProvider.get().insert(
+                TrainingPlanJoinEntity(
+                    trainingPlan = TrainingPlanEntity(
+                        0,
+                        "Название плана",
+                        DaysOfWeek.values().map { TrainingDay(it) },
+                    ),
+                    trainingPrograms = listOf()
+                )
+            )
+        }
+    }
 }

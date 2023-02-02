db173633e80daa85755f44def350daa5f626cf8d
compadre <zvvlad2@gmail.com>
2023-02-02 15:37:05 +0600

    VE: refactor EditTrainingProgramViewModel

--- app/src/main/java/com/example/trenirovochka/presentation/screens/editTrainingProgram/EditTrainingProgramViewModel.kt
+++ app/src/main/java/com/example/trenirovochka/presentation/screens/editTrainingProgram/EditTrainingProgramViewModel.kt
@@ -2,7 +2,6 @@ package com.example.trenirovochka.presentation.screens.editTrainingProgram
 
 import androidx.lifecycle.*
 import com.example.trenirovochka.domain.interactors.interfaces.ITrainingProgramsInteractor
-import com.example.trenirovochka.domain.models.TrainingProgram
 import com.example.trenirovochka.presentation.common.base.BaseViewModel
 import com.example.trenirovochka.presentation.screens.editTrainingProgram.models.EditExercise
 import com.example.trenirovochka.presentation.screens.editTrainingProgram.models.EditTrainingProgram
@@ -22,27 +21,41 @@ class EditTrainingProgramViewModel @Inject constructor(
         EditTrainingProgramFragmentArgs.fromSavedStateHandle(savedStateHandle)
     }
 
-    private val _trainingProgram: MutableLiveData<EditTrainingProgram> =
-        programsInteractor.getTrainingProgram(args.trainingProgramId).asLiveData().map {
-            it.toEditTrainingProgram()
-        } as MutableLiveData<EditTrainingProgram>
-    val trainingProgram: LiveData<EditTrainingProgram> = _trainingProgram
-    val selectedExercise: LiveData<EditExercise?> = _trainingProgram.map { program ->
+    private val _trainingProgramLD: MutableLiveData<EditTrainingProgram> = MutableLiveData()
+    val trainingProgramLD: LiveData<EditTrainingProgram> = MediatorLiveData<EditTrainingProgram>().apply {
+        addSource(programsInteractor.getTrainingProgram(args.trainingProgramId).asLiveData()) {
+            value = it.toEditTrainingProgram()
+        }
+        addSource(_trainingProgramLD) {
+            value = it
+        }
+    }
+
+    val selectedExercise: LiveData<EditExercise?> = trainingProgramLD.map { program ->
         program.exercises.find { it.isSelected }
     }
 
+    override fun exit() {
+        viewModelScope.launch {
+            updateDatabaseTrainingProgram()
+            super.exit()
+        }
+    }
+
     fun onTrainingProgramNameChanged(text: String) {
-        _trainingProgram.value?.let { editTrainingProgram ->
-            if (editTrainingProgram.name != text) {
-                updateTrainingProgram(
-                    editTrainingProgram.copy(name = text)
-                )
+        trainingProgramLD.value?.let { currentTrainingProgram ->
+            if (currentTrainingProgram.name != text) {
+                viewModelScope.launch {
+                    _trainingProgramLD.postValue(
+                        currentTrainingProgram.copy(name = text)
+                    )
+                }
             }
         }
     }
 
     fun selectExercise(selectedExercise: EditExercise) {
-        _trainingProgram.value = _trainingProgram.value?.apply {
+        _trainingProgramLD.value = trainingProgramLD.value?.apply {
             exercises = exercises.toMutableList().apply {
                 forEachIndexed { index, editExercise ->
                     if (editExercise == selectedExercise) {
@@ -56,49 +69,56 @@ class EditTrainingProgramViewModel @Inject constructor(
     }
 
     fun addExerciseToTrainingProgram(newExercise: EditExercise) {
-        _trainingProgram.value?.let { editTrainingProgram ->
-            updateTrainingProgram(
-                editTrainingProgram.copy(
-                    exercises = editTrainingProgram.exercises.toMutableList()
-                        .apply { add(newExercise) }
+        _trainingProgramLD.value?.let { currentTrainingProgram ->
+            viewModelScope.launch {
+                _trainingProgramLD.postValue(
+                    currentTrainingProgram.copy(
+                        exercises = currentTrainingProgram.exercises.toMutableList()
+                            .apply { add(newExercise) }
+                    )
                 )
-            )
+            }
         }
     }
 
     fun removeSelectedExercise() {
-        _trainingProgram.value?.let { editTrainingProgram ->
-            updateTrainingProgram(
-                editTrainingProgram.copy(
-                    exercises = editTrainingProgram.exercises.toMutableList()
-                        .apply { remove(selectedExercise.value) }
+        _trainingProgramLD.value?.let { currentTrainingProgram ->
+            viewModelScope.launch {
+                _trainingProgramLD.postValue(
+                    currentTrainingProgram.copy(
+                        exercises = currentTrainingProgram.exercises.toMutableList()
+                            .apply { remove(selectedExercise.value) }
+                    )
                 )
-            )
+            }
         }
     }
 
+    // TODO mb change
     fun editSelectedExercise(editExercise: EditExercise) {
-        _trainingProgram.value?.let { editTrainingProgram ->
-            updateTrainingProgram(
-                editTrainingProgram.copy(
-                    exercises = editTrainingProgram.exercises.toMutableList()
-                        .apply {
-                            forEachIndexed { index, eachExercise ->
-                                if (eachExercise == selectedExercise.value) {
-                                    set(index, editExercise)
+        _trainingProgramLD.value?.let { currentTrainingProgram ->
+            viewModelScope.launch {
+                _trainingProgramLD.postValue(
+                    currentTrainingProgram.copy(
+                        exercises = currentTrainingProgram.exercises.toMutableList()
+                            .apply {
+                                forEachIndexed { index, eachExercise ->
+                                    if (eachExercise == selectedExercise.value) {
+                                        set(index, editExercise)
+                                    }
                                 }
                             }
-                        }
+                    )
                 )
-            )
+            }
         }
     }
 
-    private fun updateTrainingProgram(updatedTrainingProgram: EditTrainingProgram) {
-        viewModelScope.launch {
+    private suspend fun updateDatabaseTrainingProgram() {
+        trainingProgramLD.value?.let { currentTrainingProgram ->
             programsInteractor.updateTrainingProgram(
                 args.trainingPlanId,
-                updatedTrainingProgram.toTrainingProgram()
+                currentTrainingProgram.toTrainingProgram()
             )
         }
     }

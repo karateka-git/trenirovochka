b28844e35694f3ad70489aaed6e74c818d1e1036
compadre <zvvlad2@gmail.com>
2023-02-02 15:03:26 +0600

    VE: add use and inject IODispatcher to repository

--- app/src/main/java/com/example/trenirovochka/data/local/storage/repositories/TrainingPlanRoomRepository.kt
+++ app/src/main/java/com/example/trenirovochka/data/local/storage/repositories/TrainingPlanRoomRepository.kt
@@ -4,25 +4,33 @@ import com.example.trenirovochka.data.local.storage.daos.TrainingPlanDao
 import com.example.trenirovochka.data.local.storage.entities.TrainingPlanJoinEntity
 import com.example.trenirovochka.data.local.storage.entities.TrainingProgramJoinEntity
 import com.example.trenirovochka.domain.datacontracts.storage.ITrainingPlanStorageRepository
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
+import javax.inject.Named
 
 class TrainingPlanRoomRepository @Inject constructor(
-    private val trainingPlanDao: TrainingPlanDao
+    private val trainingPlanDao: TrainingPlanDao,
+    @Named("IODispatcher") private val dispatcherIO: CoroutineDispatcher
 ) : ITrainingPlanStorageRepository {
-    override suspend fun savePlan(trainingPlanEntity: TrainingPlanJoinEntity) {
+    override suspend fun savePlan(
+        trainingPlanEntity: TrainingPlanJoinEntity
+    ) = withContext(dispatcherIO) {
         trainingPlanDao.insert(trainingPlanEntity)
     }
 
-    override suspend fun getPlans(): List<TrainingPlanJoinEntity> {
-        return trainingPlanDao.getAll()
+    override suspend fun getPlans(): List<TrainingPlanJoinEntity> = withContext(dispatcherIO) {
+        trainingPlanDao.getAll()
     }
 
     override fun getSelectedTrainingPlan(): Flow<TrainingPlanJoinEntity> {
         return trainingPlanDao.getSelectedTrainingPlan()
     }
 
-    override suspend fun updateTrainingProgram(trainingProgramJoinEntity: TrainingProgramJoinEntity) {
+    override suspend fun updateTrainingProgram(
+        trainingProgramJoinEntity: TrainingProgramJoinEntity
+    ) = withContext(dispatcherIO) {
         trainingPlanDao.insert(trainingProgramJoinEntity)
     }
 }
